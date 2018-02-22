b5cd2907a74111f1770b6223cab0d47bab162ada
leegeunhyeok <lghlove0509@naver.com>
2018-02-22 21:39:24 +0900

    Fix activity window

--- src/component/Window.vue
+++ src/component/Window.vue
@@ -68,4 +68,78 @@ export default {
   right: 0;
   overflow-y: auto;
 }
+
+
+.list-item {
+  width: 90%;
+  margin: 20px auto;
+  padding: 20px 0;
+  background-color: #fff;
+  border-radius: 20px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+}
+
+.list-item button {
+  margin-top: 10px;
+  cursor: pointer;
+  border: none;
+  outline: none;
+  box-shadow: 0px 0px 5px gray;
+  -ms-transition: 0.5s;
+  -moz-transition: 0.5s;
+  -o-transition: 0.5s;
+  -webkit-transition: 0.5s;
+  transition: 0.5s;
+}
+
+.list-item button:hover {
+  box-shadow: 1px 1px 10px gray;
+  -ms-transform: scale(1.1);
+  -moz-transform: scale(1.1);
+  -o-transform: scale(1.1);
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+
+
+.list-title {
+  margin: 10px 0px;
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: dodgerblue;
+}
+
+.list-lang {
+  color: gray;
+  padding: 0px;
+  margin: 10px 0px;
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+
+.list-image {
+  width: 50%;
+  height: 30%;
+  border-radius: 10px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+}
+
+.list-info {
+  margin: 10px 0px;
+}
+
+.list-info-area {
+  padding: 5px; 
+}
+
+@media (max-width: 768px) {
+  .list-image {
+    width: 80%;
+    height: 50%;
+  }
+}
+
+.blue {
+  color: dodgerblue;
+}
 </style>
