d45ae11851310f1616c31d908076397b1e4023dc
Leegeunhyeok <lghlove0509@naver.com>
2019-01-22 21:19:58 +0900

    Update style naming

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,31 +1,44 @@
 <template>
   <div id="main">
-    <div id="desktop">
-      <div id="shortcut-area">
-        <shortcut-icon v-for="(item, index) in shortcut" :key="index" :path="item.image" @onClick="shortcutClick(index)">
-          <div slot="shortcut-name">{{ item[$store.state.language + '-name'] }}</div>
-        </shortcut-icon>
+    <div class="desktop">
+      <div class="shortcut-area">
+        <div class="shortcut-wrap">
+          <shortcut-icon v-for="(item, index) in shortcut"
+            :key="index"
+            :path="item.image"
+            @onClick="shortcutClick(index)"
+          >
+            <div slot="shortcut-name">
+              {{ item[$store.state.language + '-name'] }}
+            </div>
+          </shortcut-icon>
+        </div>
       </div>
       <transition name="fade" mode="in-out">
-        <window-view v-if="window" :data="data" :type="type" @onClose="onClose" @shutdown="$emit('onPoweroff')" @close="window = false"></window-view>
+        <window-view v-if="window"
+          :data="data"
+          :type="type"
+          @onClose="onClose"
+          @shutdown="$emit('onPoweroff')"
+          @close="window = false"
+        />
       </transition>
     </div>
-    <div id="taskbar">
-      <button><img class="start" src="../assets/start.png" @click="showMenu = !showMenu"></button>
-      <div id="time">{{ time }}</div>
+    <div class="task-bar">
+      <button class="task-bar__menu-button">
+        <img class="task-bar__menu-button--start" src="@/assets/start.png" @click="showMenu = !showMenu">
+      </button>
+      <div class="task-bar__time">{{ time }}</div>
     </div>
     <transition name="fade" mode="in-out">
-      <div id="menu" v-if="showMenu">
-        <img src="../assets/poweroff.png" @click="$emit('onPoweroff')">
-        <br>
-        <br>
-        <b>{{ powerOff }}</b>
-        <div class="language-btn-area">
-          <div class="menu-language-text">{{changeLang}}</div>
-          <transition name="fade" mode="out-in">
-            <button class="menu-language-btn" @click="changeLanguage('kr')" v-if="$store.state.language === 'en'">Korean</button>
-            <button class="menu-language-btn" @click="changeLanguage('en')" v-else>영어</button>
-          </transition>
+      <div class="start-menu" v-if="showMenu">
+        <img class="start-menu__power-image" src="@/assets/poweroff.png" @click="$emit('onPoweroff')">
+        <b class="start-menu__power-off-text">{{ powerOff }}</b>
+        <div class="start-menu__language-button-area">
+          <div class="start-menu__language-button-area__text">{{ changeLang }}</div>
+          <button class="start-menu__language-button-area__change-button"
+            @click="changeLanguage()"
+          >{{ willChangeLanguage }}</button>
         </div>
       </div>
     </transition>
@@ -49,7 +62,8 @@ export default {
       type: 0, // 단축아이콘 타입(기본 윈도우, 브라우저, 정보)
       format: Format, // 시간포맷 변환객체
       time: Format.getSimple(null, this.$store.state.language),
-      shortcut: Shortcut
+      shortcut: Shortcut,
+      interval: null
     }
   },
   components: {
@@ -62,15 +76,22 @@ export default {
     },
     changeLang () {
       return this.$store.state.language === 'en' ? 'Change language' : '언어 변경'
+    },
+    willChangeLanguage () {
+      return this.$store.state.language === 'en' ? 'Korean' : '영어'
     }
   },
   created () {
-    this.refreshTime() // 1초에 한번씩 자동 갱신됨
+    this.refreshTime()
+  },
+  beforeDestroy () {
+    clearInterval(this.interval)
   },
   methods: {
     refreshTime () {
-      setInterval(() => {
-        this.time = this.format.getSimple(null, this.$store.state.language) // 1초마다 시간 불러오기
+      this.interval = setInterval(() => {
+        // 매 초마다 시간 갱신
+        this.time = this.format.getSimple(null, this.$store.state.language)
       }, 1000)
     },
     shortcutClick (n) { // 단축아이콘 클릭 (0~7)
@@ -87,8 +108,8 @@ export default {
       this.data = { 'title': this.shortcut[n][this.$store.state.language + '-name'] }
       this.window = true
     },
-    changeLanguage (langCode) {
-      this.$store.commit('changeLanguage', langCode)
+    changeLanguage () {
+      this.$store.commit('changeLanguage', this.$store.state.language === 'en' ? 'kr' : 'en')
     },
     onClose () { // 윈도우 닫기
       this.window = false
@@ -97,167 +118,176 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "@/assets/style/common.scss";
+
 #main {
   position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
-}
 
-#taskbar {
-  width: 100%;
-  height: 50px;
-  background-color: rgba(0, 0, 0, 0.3);
-  position: absolute;
-  left: 0;
-  bottom: 0;
-}
-
-#taskbar button {
-  cursor: pointer;
-  width: 80px;
-  height: 100%;
-  color: #fff;
-  border: none;
-  outline: none;
-  font-weight: bold;
-  font-size: 1rem;
-  background-color: rgba(0, 0, 0, 0.2);
-  -ms-transition: 0.5s;
-  -moz-transition: 0.5s;
-  -o-transition: 0.5s;
-  -webkit-transition: 0.5s;
-  transition: 0.5s;
-}
-
-#taskbar button:hover {
-  background-color: rgba(0, 0, 0, 0.4);
-}
-
-#desktop {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin-bottom: 50px;
-  overflow-y: auto;
-}
-
-#shortcut-area {
-  margin: auto;
-  margin-top: 16rem;
-  max-width: 32rem;
-  padding: 1rem;
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-}
-
-#time {
-  line-height: 50px;
-  width: 90px;
-  height: 100%;
-  color: #fff;
-  text-align: center;
-  font-size: 1rem;
-  float: right;
-  background-color: rgba(0, 0, 0, 0.2);
-}
-
-.start {
-  width: 45px;
-  height: 45px;
-  -ms-animation: fade 1s alternate infinite;
-  -moz-animation: fade 1s alternate infinite;
-  -o-animation: fade 1s alternate infinite;
-  -webkit-animation: fade 1s alternate infinite;
-  animation: fade 1s alternate infinite;
-}
-
-@keyframes fade {
-  100% {
-    -ms-transform: scale(1.2);
-    -moz-transform: scale(1.2);
-    -o-transform: scale(1.2);
-    -webkit-transform: scale(1.2);
-    transform: scale(1.2);
-  }
-}
-
-#menu {
-  height: 40rem;
-  width: 25rem;
-  border-radius: 0px 10px 0px 0px;
-  background-color: rgba(0, 0, 0, 0.5);
-  color: #fff;
-  position: absolute;
-  left: 0;
-  bottom: 50px;
-  text-align: center;
-}
-
-#menu img {
-  cursor: pointer;
-  margin-top: 30%;
-  border-radius: 50%;
-  -ms-transition: 0.5s;
-  -moz-transition: 0.5s;
-  -o-transition: 0.5s;
-  -webkit-transition: 0.5s;
-  transition: 0.5s;
-}
-
-#menu img:hover {
-  -moz-transform: scale(1.2);
-  -ms-transform: scale(1.2);
-  -o-transform: scale(1.2);
-  -webkit-transform: scale(1.2);
-  transform: scale(1.2);
-  box-shadow: 0px 0px 20px rgba(255, 255, 255, 0.5);
-}
-
-@media (max-width: 768px) {
-  #shortcut-area {
-    margin-top: 10%;
-  }
-
-  #menu {
-    border-radius: 0;
-    height: 80%;
+  .desktop {
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
+    height: 100%;
+    padding-bottom: 50px;
+    overflow-y: auto;
+
+    .shortcut-area {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100%;
+
+      .shortcut-wrap {
+        display: flex;
+        flex-wrap: wrap;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        max-width: 32rem;
+        padding: 1rem;
+      }
+    }
+  }
+
+  .task-bar {
+    width: 100%;
+    height: 50px;
+    background-color: rgba(0, 0, 0, 0.3);
+    position: absolute;
+    left: 0;
+    bottom: 0;
+
+    .task-bar__menu-button {
+      cursor: pointer;
+      width: 80px;
+      height: 100%;
+      color: #fff;
+      border: none;
+      outline: none;
+      font-weight: bold;
+      font-size: 1rem;
+      background-color: rgba(0, 0, 0, 0.2);
+      -webkit-transition: 0.5s;
+         -moz-transition: 0.5s;
+          -ms-transition: 0.5s;
+           -o-transition: 0.5s;
+              transition: 0.5s;
+
+      &:hover {
+        background-color: rgba(0, 0, 0, 0.4);
+      }
+
+      .task-bar__menu-button--start {
+        width: 45px;
+        height: 45px;
+        -webkit-animation: breath 1s alternate infinite;
+           -moz-animation: breath 1s alternate infinite;
+            -ms-animation: breath 1s alternate infinite;
+             -o-animation: breath 1s alternate infinite;
+                animation: breath 1s alternate infinite;
+      }
+    }
+
+    .task-bar__time {
+      width: 90px;
+      height: 100%;
+      color: #fff;
+      line-height: 50px;
+      text-align: center;
+      font-size: 1rem;
+      float: right;
+      background-color: rgba(0, 0, 0, 0.2);
+    }
+  }
+
+  .start-menu {
+    position: absolute;
+    left: 0;
+    bottom: 50px;
+    background-color: rgba(0, 0, 0, 0.5);
+    color: #fff;
+    text-align: center;
+
+    @media only screen and (min-width: 320px), (min-width: 768px) {
+      border-radius: 0;
+      height: 100%;
+      width: 100%;
+    }
+
+    @media only screen and (min-width: 1224px) {
+      width: 25rem;
+      height: 40rem;
+      border-top-right-radius: 10px;
+    }
+
+    .start-menu__power-image {
+      cursor: pointer;
+      margin-top: 30%;
+      margin-bottom: 2rem;
+      border-radius: 50%;
+      -webkit-transition: 0.5s;
+         -moz-transition: 0.5s;
+          -ms-transition: 0.5s;
+           -o-transition: 0.5s;
+              transition: 0.5s;
+
+      &:hover {
+        box-shadow: 0px 0px 20px rgba(255, 255, 255, 0.5);
+        -webkit-transform: scale(1.2);
+           -moz-transform: scale(1.2);
+            -ms-transform: scale(1.2);
+             -o-transform: scale(1.2);
+                transform: scale(1.2);
+      }
+    }
+
+    .start-menu__power-off-text {
+      display: block;
+    }
+
+    .start-menu__language-button-area {
+
+      .start-menu__language-button-area__text {
+        color: #fff;
+        margin-bottom: 16px;
+      }
+
+      .start-menu__language-button-area__change-button {
+        cursor: pointer;
+        outline: none;
+        border: 1px solid #fff;
+        border-radius: 5px;
+        padding: 2px 6px;
+        color: #fff;
+        background-color: transparent;
+        -webkit-transition: .3s;
+           -moz-transition: .3s;
+            -ms-transition: .3s;
+             -o-transition: .3s;
+                transition: .3s;
+
+        &:hover {
+          box-shadow: 0px 0px 10px rgba(255, 255, 255, 0.5);
+        }
+      }
+    }
   }
 }
 
-.menu-language-text {
-  color: #fff;
-  margin-bottom: 16px;
+@keyframes breath {
+  100% {
+    -webkit-transform: scale(1.1);
+       -moz-transform: scale(1.1);
+        -ms-transform: scale(1.1);
+         -o-transform: scale(1.1);
+            transform: scale(1.1);
+  }
 }
 
-.menu-language-btn {
-  background-color: rgba(0, 0, 0, 0);
-  border: 1px solid #fff;
-  border-radius: 5px;
-  outline: none;
-  padding: 2px 6px;
-  color: #fff;
-  -ms-transition: 0.5s;
-  -moz-transition: 0.5s;
-  -o-transition: 0.5s;
-  -webkit-transition: 0.5s;
-  transition: 0.5s;
-}
-
-.menu-language-btn:hover {
-  -moz-transform: scale(1.2);
-  -ms-transform: scale(1.2);
-  -o-transform: scale(1.2);
-  -webkit-transform: scale(1.2);
-  transform: scale(1.2);
-  box-shadow: 0px 0px 20px rgba(255, 255, 255, 0.5);
-}
 </style>
