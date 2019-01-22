a6732177cd2e5eaea1057130e1dace30f31c2425
Leegeunhyeok <lghlove0509@naver.com>
2019-01-22 23:21:30 +0900

    Change title text

--- src/language/IntroLanguage.js
+++ src/language/IntroLanguage.js
@@ -1,12 +1,12 @@
 export default {
-  'en': {
-    'portfolio': 'Portfolio',
-    'name': 'Leegeunhyeok',
-    'power': 'Press Power Button!'
+  en: {
+    title: 'LGH\'s PC',
+    name: 'Leegeunhyeok',
+    power: 'Press Power Button!'
   },
-  'kr': {
-    'portfolio': '포트폴리오',
-    'name': '이근혁',
-    'power': '전원버튼을 눌러주세요!'
+  kr: {
+    title: '이근혁의 PC',
+    name: '이근혁',
+    power: '전원버튼을 눌러주세요!'
   }
 }
