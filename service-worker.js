971474e66dac31ab0e6f7230e74f60136862dbe7
Leegeunhyeok <lghlove0509@naver.com>
2019-01-22 23:24:27 +0900

    Add service-worker.js tamplate

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const _version = 'v1';
+const cacheName = 'v1';
+const cacheList = []
+
+self.addEventListener('install', event => {
+  
+})
+
+self.addEventListener('activate', event => {
+  
+})
+
+self.addEventListener('fetch', event => {
+
+})
