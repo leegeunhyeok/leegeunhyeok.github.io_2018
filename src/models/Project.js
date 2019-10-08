df902537c23fbac06b66089e4818a90b2714d7ce
Minha <Minha@DESKTOP-26EKF6F>
2019-10-09 02:06:12 +0900

    minha

--- src/models/Project.js
+++ src/models/Project.js
@@ -1,15 +1,32 @@
 const directory = '/img/project/'
 
 export default [
+
+	  {
+	    'en': {
+	      'name': 'Dating Simulation Game',
+	      'info': 'For academy friends(mini Project)',
+	      'disc': 'Game for anyone who wants to practice romance'
+	    },
+	    'kr': {
+	      'name': '경현 연애 시뮬레이션',
+	      'info': '연애 시뮬레이션 게임(미니 프로젝트)',
+	      'disc': '연애를 연습해보고 싶은 사람을 위한 게임(for 학원친구)'
+	    },
+	    'image': directory + 'test.png',
+	    'FrameWork': 'Spring',
+	    'date': '2019-04-26 ~ 2019-05-07',
+	    'link': 'https://github.com/DonghoLim/miniproject1'
+	  },
   {
     'en': {
       'name': 'COC (Create or Choice)',
-      'info': 'Seoul Travel course platform',
+      'info': 'Seoul Travel course platform(semi Project)',
       'disc': 'Enjoy Your Travel!'
     },
     'kr': {
       'name': 'COC (Create or Choice)',
-      'info': '서울 여행 계획 플랫폼입니다',
+      'info': '서울 여행 계획 플랫폼입니다(세미 프로젝트)',
       'disc': '서울여행을 즐겨보세요'
     },
     'image': directory + 'coc2.png',
@@ -20,12 +37,12 @@ export default [
   {
     'en': {
       'name': 'Drawing Your Cloud',
-      'info': 'Reward-type Crowdfunding',
+      'info': 'Reward-type Crowdfunding(final Project)',
       'disc': 'Reward-type crowdfunding that focuses on user convenience'
     },
     'kr': {
       'name': '니가 그린 구름 그림',
-      'info': '리워드형 크라우드펀딩 사이트',
+      'info': '리워드형 크라우드펀딩 사이트(파이널 프로젝트)',
       'disc': '사용자의 편의에 중점을 둔, 리워드형 크라우드 펀딩'
     },
     'image': directory + 'test.png',
